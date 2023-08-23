b0f094bf0af93011e2a337ef36c2cb4a0c007b60
mansouriyassine <mansouri.yassine1@gmail.com>
2023-08-23 15:26:10 -0700

    new test

--- shell.c
+++ shell.c
@@ -1,27 +1,26 @@
 #include "shell.h"
 #include "helpers.h"
 
+/**
+ * main - Entry point for the simple shell program.
+ *
+ * Return: EXIT_SUCCESS on successful completion.
+ */
 int main(void)
 {
 char *line;
 char **args;
 int status;
 
-do
-{
+do {
 printf("#cisfun$ ");
-        
 line = read_line();
-
 args = parse_line(line);
-
 status = execute_command(args);
 
 free(line);
 free(args);
 
-}
-while (status);
-
+} while (status);
 return (EXIT_SUCCESS);
 }
